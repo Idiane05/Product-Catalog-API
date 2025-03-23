9986af155d92c9d7bb7c2455b2ae389e83c89ef8
Diane INGABIRE <127386345+Idiane05@users.noreply.github.com>
2025-03-23 22:13:34 +0200

    added

---
+++ models/Product.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const ProductSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    description: { type: String },
+    price: { type: Number, required: true },
+    category: { type: String, required: true },
+    stock: { type: Number, required: true },
+    variants: [{ size: String, color: String }]
+}, { timestamps: true });
+
+module.exports = mongoose.model('Product', ProductSchema);
